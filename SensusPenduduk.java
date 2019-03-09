f6e727167f1ecf8fa7d952071d8676d965bb3fdc
fauziahnurul <48358818+fauziahnurul@users.noreply.github.com>
2019-03-09 21:01:53 +0700

    Add files via upload

---
+++ SensusPenduduk.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sensus.penduduk;
+
+/**
+ *
+ * @author axioo
+ */
+public class SensusPenduduk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
